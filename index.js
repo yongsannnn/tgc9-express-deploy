0576ff082f2398b2302ad8765fbeb2de4a356bfa
kunxin-chor <chorkunxin@yahoo.com>
2020-12-08 03:32:15 +0000

    Push refactoring

--- index.js
+++ index.js
@@ -1,52 +1,17 @@
 // EXPRESS AND OTHER SETUP
 const express = require('express');
+const {setupExpressApp} = require('./setupExpress');
+const {setupHBS} = require('./setupHBS');
 const MongoUtil = require('./MongoUtil.js')
 const ObjectId = require('mongodb').ObjectId;
-const hbs = require('hbs')
-const wax = require('wax-on')
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
-const flash = require('connect-flash')
-// allows us to inject into the environment (the OS) our environmental variabkes
-require('dotenv').config();
 
-// load in environment variables
+// allows us to inject into the environment (the OS) our environmental variabkes
 require('dotenv').config();
 
 // create the app
 const app = express();
-// use handlebars as the view engine (for templates) -- because there many other choices
-app.set('view engine', 'hbs')
-// we want our static files (images, css etc.) to be in a folder named public
-app.use(express.static('public'))
-// allows express to data submitted via forms
-app.use(express.urlencoded({extended:false}))
-
-
- app.use(cookieParser("secret"))
- app.use(session({
-     'cookie': {
-         maxAge: 60000
-     }
- }))
- app.use(flash())
-
-// register a middleware for the flash message
-app.use(function(req, res, next){
-    res.locals.success_messages = req.flash('success_messages');
-    res.locals.error_messages = req.flash('error_messages');
-    next();
-})
-
-// setup template inheritance
-wax.on(hbs.handlebars);
-wax.setLayoutPath('./views/layouts')
-
-// handlebar helpers
- var helpers = require("handlebars-helpers")({
-    handlebars: hbs.handlebars
-  });
-
+setupExpressApp(app);
+setupHBS();
 
 async function main() {
     const MONGO_URL=process.env.MONGO_URL;
